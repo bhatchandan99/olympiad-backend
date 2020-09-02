09fcfa1983659bff2b69e2763b18eb3c70744107
Ayushi Goyal <ashigoyal05@gmail.com>
2020-09-02 21:21:59 +0530

    class 2 img

--- online_test/settings.py
+++ online_test/settings.py
@@ -138,3 +138,5 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'hayaabidi2000@gmail.com'
 EMAIL_HOST_PASSWORD = 'hayaZillion'
+# 'ayukumawat1999@gmail.com'
+# EMAIL_HOST_PASSWORD = 'kumawat1999'
